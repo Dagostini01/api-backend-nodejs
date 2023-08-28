54e3eb4bfe99efd22e4bac7dabafd4aa5e94db3e
Pedro Dagostini <87324617+Dagostini01@users.noreply.github.com>
2023-08-27 22:41:03 -0300

    token ativo

--- index.js
+++ index.js
@@ -14,17 +14,44 @@ const lista = [
     {nome: "Cesar Augusto Ferreira", telefone:"98771-0258", email: "cesar.augusto@gmail.com"}
 ]
 
-app.route("/login").get((request, response) => {
-    const token = jwt.sign("pedro", "ABCD12345", {})
+const filtroJwt = (request, response, next) => {
+    console.log("entrou no filtro")
+    let jwtOk = false
+    let token = null
+    const authorization = request.headers['authorization']
+    console.log(authorization)
+
+    if (authorization) {
+        token = authorization.split(" ")[1]
+        console.log(token)
+    }
+
+    if(token){
+        jwt.verify(token, "ABC123", (err, payload) => {
+            if(payload){
+                next()
+            } else {
+                response.status(401).send("Token inválido")
+            }
+        })
+    } else {
+        return response.status(401).send("Falta informações de autorização")
+    }
+
+}
+
+app.route("/login").get( (request, response) => {
+    const payload = {user: "pedro", profile: "admin"}
+    const token = jwt.sign(JSON.stringify(payload), "ABC123", {})
     const resposta = {usuario: "pedro", token}
     response.status(200).json(resposta)
 })
 
-app.route("/").get((request, response)=>{
+app.route("/").get(filtroJwt, (request, response)=>{
     response.status(200).json(lista)
 })
 
-app.route("/").post((request, response)=>{
+app.route("/").post(filtroJwt, (request, response)=>{
     lista.push(request.body)
     response.status(200).send("Contato cadastrado")
 })
